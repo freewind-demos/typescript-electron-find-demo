a5173fea42e5d74b8201d5a4ab6cc091486c759b
Freewind <nowindlee@gmail.com>
2020-04-07 15:48:44 +0800

    typescript-electron-find-demo

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,17 @@
 import {app, BrowserWindow} from 'electron';
 
+const electronDebug = require('electron-debug');
+
 console.log('main process', __dirname);
 
+electronDebug();
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   const rendererUrl = 'file://' + __dirname + '/renderer/index.html';
   console.log('rendererUrl', rendererUrl);
   mainWindow.loadURL(rendererUrl);
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,5 +1,15 @@
 import './index.css';
 
+import {remote} from 'electron';
+import keymaster from 'keymaster';
+
 console.log('renderer process');
 
 document.getElementById('content')!.innerText = 'hello from javascript';
+
+const {FindInPage} = require('electron-find');
+const findInPage = new FindInPage(remote.getCurrentWebContents());
+
+keymaster('command+f', () => {
+  findInPage.openFindWindow();
+});
